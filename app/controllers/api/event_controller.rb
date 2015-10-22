674f7dd99c480d3172d96a07d86dfbd5a62e5e68
Dasha <dashka.prostokvashka@gmail.com>
2015-10-22 14:21:12 +0300

    adding issues

--- app/controllers/api/event_controller.rb
+++ app/controllers/api/event_controller.rb
@@ -1,9 +1,14 @@
 class Api::EventController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => [:recieve]
+  skip_before_filter :authorize
 
-  def create
+  def recieve
+    @issue = Issue.find_or_initialize_by(id: params[:issue_id])
 
+    @issue.update_attributes(estimated_hours: params[:estimated_hours],
+                                                        hours: params[:hours],
+                                                        status: params[:status],
+                                                        subject: params[:issue_subject])
   end
-
-  private
 
 end
--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -4,7 +4,7 @@ class Sprint < ActiveRecord::Base
   has_many :issues
   validates :name, uniqueness: true, presence: true
   validates :clients, presence: true
-  # validates :state,  inclusion: { in: %w(true)} (?) , :on => :update - пока оставлю
+  #validates :state,  format: { with: /\Afalse+\z/, message: "Sprint alredy closed"}, :on => :update # с ним часы не считаются
   validate :open_sprint
   accepts_nested_attributes_for :clients
 
