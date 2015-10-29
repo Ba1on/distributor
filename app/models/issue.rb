151403e6b133510ba3c1514d61f0e5548f85f2d5
Dasha <dashka.prostokvashka@gmail.com>
2015-10-29 10:17:07 +0300

    progress bar

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,7 +4,7 @@ class Issue < ActiveRecord::Base
 
   def issue_hours
     opened = Sprint.where(state: true).first
-    errors.add(:issue_id, I18n.t(:too_many_hours)) if opened.nil? || opened.estimated_hours_to_sprint +
-      estimated_hours > opened.work_hours
+    errors.add(:issue_id, I18n.t(:too_many_hours)) if (opened.nil? || opened.estimated_hours_to_sprint +
+      estimated_hours > opened.work_hours) && sprint_id
   end
 end
